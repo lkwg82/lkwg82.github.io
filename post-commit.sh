ba3948733dd76187d8ad5ebfb9769148f07a1aa7
Lars K.W. Gohlke <lkwg82@gmx.de>
2014-01-12 15:18:43 +0100

    add post commit for version update

--- post-commit.sh
+++ post-commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set +x
 
 baseDir=$(git rev-parse --show-toplevel)
 file=$baseDir/index.html
@@ -8,4 +9,4 @@ file=$baseDir/index.html
 version =$(git log  -n1 --format=format:"%H %ci")
 
 sed -e 's#<span class="version">.*</span>#<span class="version">'"$version"'</span>#' -i $file
-git commit -m 'updated version info' $file
+git --no-verify commit -m 'updated version info' $file
