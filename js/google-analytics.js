e50776f370dd538afc3473874efba78034000b53
Lars K.W. Gohlke <lkwg82@gmx.de>
2014-02-02 06:13:07 +0100

    added ga js error tracking

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -6,6 +6,10 @@ _gaq.push(['_gat._anonymizeIp']);
 _gaq.push(['_setSiteSpeedSampleRate', 100]);
 _gaq.push(['_trackPageview']);
 
+window.onerror = function(message, file, line) {
+   _gaq.push(['_trackEvent', 'JS Error', file + ':' + line + '\n\n' + message]);
+};
+
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
