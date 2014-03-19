6ed86afd83275f5653b764d161cba8ee66043b6f
Lars K.W. Gohlke <lkwg82@gmx.de>
2014-03-19 21:00:26 +0100

    added dynamic video resizing

--- js/decorate-links.js
+++ js/decorate-links.js
@@ -1,22 +1,34 @@
 $(function() {
-window.setTimeout( function(){
-	$('img').each(function(){
-		if ( $(this).attr('data-delayed-src') ){
-			$(this).attr('src', $(this).attr('data-delayed-src'));
+	window.setTimeout( function(){
+		$('img').each(function(){
+			if ( $(this).attr('data-delayed-src') ){
+				$(this).attr('src', $(this).attr('data-delayed-src'));
+			}
+		});
+	},0);
+
+	$( "#header a" ).each(function(){
+		var linkObject= this;
+		var link = $(linkObject).attr('href');
+
+		if ( link.match(/^#.*/) ){
+			$(linkObject).attr("href",'#');
+
+			$( this ).click(function(){
+				$(link).animatescroll({scrollSpeed:2000,easing:'easeOutCirc'});
+			});
 		}
 	});
-},0);
 
-  $( "#header a" ).each(function(){
-	var linkObject= this;
-	var link = $(linkObject).attr('href');
-
-	if ( link.match(/^#.*/) ){
-		$(linkObject).attr("href",'#');
-
-		$( this ).click(function(){
-			$(link).animatescroll({scrollSpeed:2000,easing:'easeOutCirc'});
-		});
+	function resizeVideo(){
+		var video = document.getElementById("my_video_1");
+		video.width = Math.min(window.innerWidth - video.getBoundingClientRect().left*2, 640);
+		video.height = video.width / 1.35;
 	}
-  });
+
+	resizeVideo();
+
+	$( window ).resize(function() {
+		resizeVideo();
+	});
 });
