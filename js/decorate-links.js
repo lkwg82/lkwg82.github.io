d6b72bcb27cb434c02b80843235a169ce06c1ae2
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-03-31 00:40:36 +0200

    removed videojs and added timing tracking

--- js/decorate-links.js
+++ js/decorate-links.js
@@ -1,33 +1,17 @@
 $(function() {
-
-	$( "#header a" ).each(function(){
-		var linkObject= this;
+	$("#header a").each(function() {
+		var linkObject = this;
 		var link = $(linkObject).attr('href');
 
-		if ( link.match(/^#.*/) ){
-			$(linkObject).attr("href",'#');
+		if (link.match(/^#.*/)) {
+			$(linkObject).attr("href", '#');
 
-			$( this ).click(function(){
-				$(link).animatescroll({scrollSpeed:2000,easing:'easeOutCirc'});
+			$(this).click(function() {
+				$(link).animatescroll({
+					scrollSpeed: 2000,
+					easing: 'easeOutCirc'
+				});
 			});
 		}
 	});
-
-	function resizeVideo(){
-		var video = document.getElementById("my_video_1");
-		video.width = Math.min(window.innerWidth - video.getBoundingClientRect().left*2, 640);
-		video.height = video.width / 1.35;
-	}
-
-	resizeVideo();
-
-	$( window ).resize(function() {
-		resizeVideo();
-	});
-
-	// TODO
-	//~ var supportsOrientationChange = "onorientationchange" in window;
-	//~ if ( supportsOrientationChange ){
-		//~ window.addEventListener("orientationchange", function() {resizeVideo();}, false);
-	//~ }
 });
--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,188 +1,208 @@
 var host = window.location.hostname;
-var isLocalhost =  (host === 'localhost' || host === '127.0.0.1');
+var isLocalhost = (host === 'localhost' || host === '127.0.0.1');
 
-if ( isLocalhost ){
-    window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
-        if (lineNumber === 0) {
-            console.log("script from other domain throw an error");
-            // see http://stackoverflow.com/questions/5913978/cryptic-script-error-reported-in-javascript-in-chrome-and-firefox
-        } else {
-
-            alert('Error:\n ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber
-                    + ' Column: ' + column + ' StackTrace: ' + errorObj);
-        }
-    };
+if (isLocalhost) {
+	window.onerror = function(errorMsg, url, lineNumber, column, errorObj) {
+		if (lineNumber === 0) {
+			console.log("script from other domain throw an error");
+			// see http://stackoverflow.com/questions/5913978/cryptic-script-error-reported-in-javascript-in-chrome-and-firefox
+		} else {
+			alert('Error:\n ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber +
+				' Column: ' + column + ' StackTrace: ' + errorObj);
+		}
+	};
 }
-else{
+//else 
+{
+
+	(function(i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function() {
+			(i[r].q = i[r].q || [])
+			.push(arguments)
+		}, i[r].l = 1 * new Date();
+		a = s.createElement(o),
+			m = s.getElementsByTagName(o)[0];
+		a.async = 1;
+		a.src = g;
+		m.parentNode.insertBefore(a, m)
+	})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+	ga('create', 'UA-39213747-1', {
+		'siteSpeedSampleRate': 100
+	});
+	ga('set', 'forceSSL', true);
+	ga('set', 'setDomainName', 'www.lgohlke.de');
+	ga('set', 'anonymizeIp', true);
+	ga('send', 'pageview');
+
+	window.onerror = function(message, file, line) {
+		ga('send', 'event', {
+			'eventCategory': 'JS Error',
+			'eventValue': file + ':' + line + '\n\n' + message
+		});
+	};
+
+	$(function() {
+		function _track(data) {
+			ga('send', 'event', data);
+		}
+
+		function track(category, action, opt_label, opt_value) {
+			console.debug("track " + category + ", " + action + ", " + opt_label + ", " + opt_value);
+			if (typeof opt_label === "undefined") {
+				_track({
+					'eventCategory': category,
+					'eventAction': action
+				});
+			} else if (typeof opt_value === "undefined") {
+				_track({
+					'eventCategory': category,
+					'eventAction': action,
+					'eventLabel': opt_label
+				});
+			} else {
+				_track({
+					'eventCategory': category,
+					'eventAction': action,
+					'eventLabel': opt_label,
+					'event': opt_value
+				});
+			}
+		}
+
+		function onClick(object, category, action, opt_label, opt_value) {
+			$(object).click(function() {
+				track(category, action, opt_label, opt_value);
+			});
+		}
+
+		$("a").each(function() {
+			var linkObject = this;
+			var link = $(linkObject).attr('href');
+
+			if (link.match(/^#.*/)) {
+				onClick(this, 'onpage-links', 'navigate', $(linkObject).text() + ' (' + link + ')');
+			} else if (link.match(/^https?:\/\/.*/)) {
+				onClick(this, 'offpage-links', 'navigate', $(linkObject).text() + ' (' + link + ')');
+			} else if (link.match(/.*pdf$/i)) {
+				onClick(this, 'downloads', 'download', $(linkObject).text() + ' (' + link + ')');
+			}
+
+			if (link.match(/^mailto\:/i)) {
+				onClick(this, 'onpage-links', 'contact', 'mail');
+			} else if (link.match(/^https:\/\/plus.google.com.*/)) {
+				onClick(this, 'onpage-links', 'contact', 'G+');
+			}
+		});
+
+		$(document).mouseup(function() {
+			var selectedText = window.getSelection();
+
+			//~ console.debug(selectedText);
+
+			var nodeText = selectedText.focusNode.data;
+			var maxLength = Math.min(10, nodeText.length);
+			var snippet = nodeText.substr(0, maxLength);
+
+			if (selectedText.type == "Range") {
+				track("view", 'selected text', snippet + " [" + selectedText + "]");
+			} else if (selectedText.type == "Caret") {
+				track("view", 'click', snippet);
+			}
+		});
+
+		function isElementInViewport(el) {
+			var rect = el.getBoundingClientRect();
+
+			var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+			var windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+			var visibleY = rect.bottom > 0 && rect.top <= windowHeight;
+			var visibleX = rect.right > 0 && rect.left <= windowWidth;
+
+			return visibleY && visibleX;
+		}
+
+		//~ var started;
+		//~ $.data(this, 'focusTimer', setTimeout(function() {
+		//~ var timerName = 'watchTimer';
+		//~ var window_focus;
+
+		//~ $(window).focus(function() {
+		//~ window_focus = true;
+		//~ }).blur(function() {
+		//~ window_focus = false;
+		//~ });
+
+		//~ if ( window_focus && !started){
+		//~ $.data(this, timerName, setTimeout(function() {
+		//~ $(".trackView").each(function(){
+		//~ var trackView = this;
+		//~ var id = $(trackView).attr('id');
+
+		//~ if ( isElementInViewport(trackView)){
+		//~ track("view","watch",id);
+		//~ }
+		//~ });
+		//~ started = true;
+		//~ }))
+		//~ }
+		//~ else{
+		//~ if (!window_focus){
+		//~ clearTimeout($.data(this, timerName));
+		//~ }
+		//~ }
+		//~ },500));
 
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-39213747-1']);
-    _gaq.push(['_setDomainName', 'www.lgohlke.de']);
-    _gaq.push(['_gat._forceSSL']);
-    _gaq.push(['_gat._anonymizeIp']);
-    _gaq.push(['_setSiteSpeedSampleRate', 100]);
-    _gaq.push(['_trackPageview']);
 
-    window.onerror = function(message, file, line) {
-       _gaq.push(['_trackEvent', 'JS Error', file + ':' + line + '\n\n' + message]);
-    };
+		var areasSeen = new Array();
 
-    (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+		$(window).scroll(function() {
+			clearTimeout($.data(this, 'scrollTimer'));
+			$.data(this, 'scrollTimer', setTimeout(function() {
 
-    $(function() {
-      function track(category, action, opt_label, opt_value, opt_noninteraction){
-        console.debug("track " + category + ", " + action + ", " + opt_label + ", " + opt_value);
-        if (typeof opt_label === "undefined")
-        {
-          _gaq.push(['_trackEvent', category, action]);
-        }
-        else if (typeof opt_value === "undefined")
-        {
-          _gaq.push(['_trackEvent', category, action, opt_label]);
-        }
-        else if (typeof opt_noninteraction === "undefined")
-        {
-          _gaq.push(['_trackEvent', category, action, opt_label, opt_value]);
-        }
-        else{
-          _gaq.push(['_trackEvent', category, action, opt_label, opt_value, opt_noninteraction]);
-        }
-      }
+				var areasNowSeen = new Array();
 
-      function onClick(object, category, action, opt_label, opt_value, opt_noninteraction){
-        $( object ).click(function(){ track(category,action,opt_label,opt_value,opt_noninteraction); });
-      }
+				$(".trackView")
+					.each(function() {
+						var trackView = this;
+						var id = $(trackView)
+							.attr('id');
 
-      $("a" ).each(function(){
-        var linkObject= this;
-        var link = $(linkObject).attr('href');
+						if (!id) {
+							var rect = trackView.getBoundingClientRect();
+							// console.log("missing id at " + rect.top + "x" + rect.left +									" see " + $(trackView)									.text());
+						} else {
+							if (isElementInViewport(trackView)) {
+								//~ console.log(" visible " + id);
+								areasNowSeen.push(id);
+							}
+						}
+					});
 
-        if ( link.match(/^#.*/) ){
-          onClick(this,'onpage-links','navigate',$(linkObject).text() + ' ('+link+')');
-        }else if( link.match(/^https?:\/\/.*/)){
-          onClick(this,'offpage-links','navigate',$(linkObject).text() + ' ('+link+')');
-        }else if ( link.match(/.*pdf$/i) ){
-          onClick(this,'downloads','download',$(linkObject).text() + ' ('+link+')');
-        }
+				var newlySeen = new Array();
 
-        if ( link.match(/^mailto:/i) ){
-          onClick(this,'onpage-links','contact','mail');
-        }else if ( link.match(/^https:\/\/plus.google.com.*/) ){
-          onClick(this,'onpage-links','contact','G+');
-        }
-      });
+				for (var i = 0; i < areasNowSeen.length; i++) {
+					var seen = false;
+					for (var x = 0; x < areasSeen.length; x++) {
+						if (!seen && areasSeen[x] == areasNowSeen[i]) {
+							seen = true;
+						}
+					}
 
-      $(document).mouseup( function(){
-        var selectedText = window.getSelection();
+					if (!seen) {
+						newlySeen.push(areasNowSeen[i]);
+					}
+				}
 
-        //~ console.debug(selectedText);
+				for (var i = 0; i < newlySeen.length; i++) {
+					track("view", "item", newlySeen[i]);
+				}
 
-        var nodeText=selectedText.focusNode.data;
-        var maxLength = Math.min(10,nodeText.length);
-        var snippet = nodeText.substr(0, maxLength);
-
-        if ( selectedText.type == "Range" ){
-          track("view",'selected text',snippet + " [" + selectedText + "]");
-        }else if(selectedText.type == "Caret"){
-          track("view",'click',snippet);
-        }
-      });
-
-      function isElementInViewport (el) {
-        var rect = el.getBoundingClientRect();
-
-        var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-        var windowWidth = (window.innerWidth || document.documentElement.clientWidth);
-
-        var visibleY = rect.bottom > 0 &&  rect.top <= windowHeight;
-        var visibleX = rect.right > 0 &&  rect.left <= windowWidth;
-
-        return visibleY && visibleX;
-      }
-
-      //~ var started;
-      //~ $.data(this, 'focusTimer', setTimeout(function() {
-        //~ var timerName = 'watchTimer';
-        //~ var window_focus;
-
-        //~ $(window).focus(function() {
-          //~ window_focus = true;
-        //~ }).blur(function() {
-          //~ window_focus = false;
-        //~ });
-
-        //~ if ( window_focus && !started){
-          //~ $.data(this, timerName, setTimeout(function() {
-            //~ $(".trackView").each(function(){
-              //~ var trackView = this;
-              //~ var id = $(trackView).attr('id');
-
-              //~ if ( isElementInViewport(trackView)){
-                //~ track("view","watch",id);
-              //~ }
-            //~ });
-            //~ started = true;
-          //~ }))
-        //~ }
-        //~ else{
-          //~ if (!window_focus){
-            //~ clearTimeout($.data(this, timerName));
-          //~ }
-        //~ }
-      //~ },500));
-
-
-
-      var areasSeen = new Array();
-
-      $(window).scroll(function() {
-            clearTimeout($.data(this, 'scrollTimer'));
-            $.data(this, 'scrollTimer', setTimeout(function() {
-
-          var areasNowSeen = new Array();
-
-          $(".trackView").each(function(){
-            var trackView = this;
-            var id = $(trackView).attr('id');
-
-            if ( !id){
-              var rect = trackView.getBoundingClientRect();
-              console.log("missing id at " + rect.top + "x"+rect.left + " see " + $(trackView).text());
-            }
-            else{
-              if ( isElementInViewport(trackView)){
-                //~ console.log(" visible " + id);
-                areasNowSeen.push(id);
-              }
-            }
-          });
-
-          var newlySeen = new Array();
-
-          for(var i = 0; i < areasNowSeen.length;i++){
-            var seen = false;
-            for(var x = 0; x < areasSeen.length;x++){
-              if (!seen && areasSeen[x] == areasNowSeen[i]){
-                seen = true;
-              }
-            }
-
-            if (!seen){
-              newlySeen.push(areasNowSeen[i]);
-            }
-          }
-
-          for(var i = 0; i < newlySeen.length;i++){
-            track("view","item",newlySeen[i]);
-          }
-
-          areasSeen = areasNowSeen;
-            }, 250));
-      });
-    })
+				areasSeen = areasNowSeen;
+			}, 250));
+		});
+	})
 }
