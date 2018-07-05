d3fc427706581af5158844be64667c2f415940f2
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-07-06 00:58:39 +0200

    moved update-version-info.sh into run.sh

--- run.sh
+++ run.sh
@@ -94,7 +94,7 @@ case "$1" in
     ;;
     "deploy")
         $0 docker  npm install
-        ./update-version-info.sh
+        $0 update-version-info
         $0 docker  grunt
 
         pushd www
@@ -126,6 +126,18 @@ case "$1" in
     "test")
        echo "running tests"
        runTests
+    ;;
+    "update-version-info")
+        file=index.html
+
+        version=$(git log  -n1 --format=format:"%ci %H")
+        message="updated version info"
+
+        if [ "$(git log  -n1 --format=format:"%s")" != "$message" ] ;then
+
+            sed -e 's#<span class="version">.*</span>#<span class="version">last changed '"$version"'</span>#' -i ${file}
+            git commit --no-verify -m 'updated version info' ${file}
+        fi
     ;;
     *)
         cat << EndOfMessage
